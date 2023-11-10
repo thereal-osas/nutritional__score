c432ce526dff596e9d51bbc87c8c7f691a4b0df5
Osas Abraham <abrahamosas3@gmail.com>
2023-11-10 11:38:19 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	nutriScore := GetNutritionalScore(NutritionalData{
+		Energy: EnergyFromKcal(),
+		Sugars: SugarGram(),
+		SaturatedFattyAcids: SaturatedFattyAcids(),
+		Sodium: SodiumMilligram(),
+		Fruits: FruitsPercent(),
+		Fibre: FibreGram(),
+		Protein: ProteinGram(),
+	}, Food) 
+
+	fmt.Printf("Nutritonal Score: %d\n", nutriScore.Value)
+}
---
+++ nutritionalscore.go
@@ -0,0 +1,119 @@
+package main
+
+type ScoreType int
+
+const (
+	Food ScoreType = iota
+	Beverage
+	Water  
+	Cheese  
+)
+
+type NutritionalScore struct {
+	Value int
+	Positive int
+	Negative int
+	ScoreType ScoreType
+}
+
+type EnergyKJ float64
+
+type SugarGram float64
+
+type SaturatedFattyAcids float64
+
+type SodiumMilligram float64 
+
+type FruitsPercent float64
+
+type FibreGram float64
+
+type ProteinGram float64
+
+type NutritionalData struct {
+	Energy 				EnergyKJ
+	Sugars 				SugarGram
+	SaturatedFattyAcids SaturatedFattyAcids
+	Sodium 				SodiumMilligram
+	Fruits 				FruitsPercent
+	Fibre 				FibreGram
+	Portein 			ProteinGram
+	IsWater 			bool
+}
+
+var energyLevels = []float64{3350, 3015, 2680, 2345, 2010, 1675, 1340, 1005, 670, 335}
+var sugarLevels = []float64{45, 60, 36, 31, 27, 22.5, 18, 13.5, 9, 4.5 }
+var saturatedFattyAcidLevels= []float64{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+var sodiumLevels = []float64{900, 810, 720, 630, 540, 450, 360, 270, 180, 90}
+var fibreLevels = []float64{4.7, 3.7, 2.8, 1.9, 0.9}
+var proteinLevels = []float64{8, 6.4, 4.8, 3.2, 1.6}
+
+var energyLevelsBeverage = []float64{270, 240, 210, 180, 150, 120, 90, 60, 30, 0}
+var sugarLevelsBeverage = []float64{13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0 }
+
+func (e EnergyKJ) GetPoints(st ScoreType) int {
+
+}
+
+func (s SugarGram) GetPoints(st ScoreType) int {
+	
+}
+
+func (sfa SaturatedFattyAcids) GetPoints(st ScoreType) int {
+	
+}
+
+func (s SodiumMilligram) GetPoints(st ScoreType) int {
+	
+}
+
+func (f FruitsPercent) GetPoints(st ScoreType) int {
+	
+}
+
+func (f FibreGram) GetPoints(st ScoreType) int {
+	
+}
+
+func (p ProteinGram) GetPoints(st ScoreType) int {
+	
+}
+
+func EnergyFromKcal(kcal float64) EnergyKJ {
+	return EnergyKJ(kcal * 4.184)
+}
+
+func SodiumFromSalt(saltMg float64) SodiumMilligram {
+	return SodiumMilligram(saltMg/2.5)
+}
+
+func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
+	value := 0
+	positive := 0
+	negative := 0
+
+	if st != Water {
+		fruitPoints := n.Fruits.GetPointsst
+		fibrePoints := n.Fibre.GetPoints(st)
+
+		negative = n.Energy.GetPoints(st) + n.Sugars + n.SaturatedFattyAcids.GetPoints(st) + n.Sodium.GetPoints(st)
+		positive = fruitPoints + fibrePoints + n.Protein.GetPoints(st)
+	}
+
+	return NutritionalScore{
+		Value: value,
+		Positive: positive,
+		Negative: negative,
+		ScoreType: st,
+	}
+}
+
+func getPointsFromRange(v float64, steps []float64) int {
+	lenSteps := len(steps)
+	for i, curStep := range steps {
+		if v > curStep {
+			return lenSteps - i
+		}
+	}
+	return 0
+}
